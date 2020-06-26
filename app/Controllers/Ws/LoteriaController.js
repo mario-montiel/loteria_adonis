7f41450af1af733d9ee8161440faca48598d87b4
Juan Angel <juangel2406@gmail.com>
2020-06-26 13:49:28 -0500

    websocket install

--- app/Controllers/Ws/LoteriaController.js
+++ app/Controllers/Ws/LoteriaController.js
@@ -1,9 +1,17 @@
 'use strict'
+const User = use('App/Models/User')
 
 class LoteriaController {
   constructor ({ socket, request }) {
     this.socket = socket
     this.request = request
+  }
+
+  async onJoin(id) {
+    //let user = await User.find('username', data.username)
+    //if (user) {
+
+    //}
   }
 }
 
