30f22e3fc112899ecb5a601ba2181ea030a1e61e
Juan Angel <juangel2406@gmail.com>
2020-06-26 15:51:33 -0500

    game status

--- database/migrations/1592524484145_games_schema.js
+++ database/migrations/1592524484145_games_schema.js
@@ -7,6 +7,7 @@ class GamesSchema extends Schema {
   up () {
     this.create('games', (table) => {
       table.increments()
+      table.string('status', 8).defaultTo('inactive') // active | inactive
     })
   }
 
