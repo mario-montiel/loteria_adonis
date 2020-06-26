db866d3ed89c9eb94826be474f2b27ec570b3daf
Juan Angel <juangel2406@gmail.com>
2020-06-26 17:02:33 -0500

    onJoin estrcucture progress

--- app/Controllers/Ws/LoteriaController.js
+++ app/Controllers/Ws/LoteriaController.js
@@ -1,5 +1,6 @@
 'use strict'
 const User = use('App/Models/User')
+const Game = use('App/Models/Game')
 
 class LoteriaController {
   constructor ({ socket, request }) {
@@ -8,10 +9,49 @@ class LoteriaController {
   }
 
   async onJoin(id) {
-    //let user = await User.find('username', data.username)
-    //if (user) {
+    let user = await User.find(id)
+    if (!user) { return }
 
-    //}
+    user.status = 'active'
+    await user.save()
+
+    // BROADCAST connected user
+    this.socket.broadcast('user', user)
+
+    let game = await Game.first()
+    if (game) {
+      let status = game.status
+
+      // in case the game is waiting for users
+      if (status == 'preparing') {
+        let activeUsers = await User.query() .where('status', 'active').getCount()
+        if (activeUsers > 1) {
+          //function myFunc(arg) {
+            //console.log(`arg was => ${arg}`);
+          //}
+
+          //setTimeout(myFunc, 30000, 'funky');
+        }
+      }
+      // in case the game is in progress
+      else if (status == 'playing') {
+
+      }
+    }
+    else {
+      game = new Game()
+      game.status = 'preparing'
+    }
+
+    //this.socket.broadcast()
+  }
+
+  async onCheck() {
+
+  }
+
+  async onClose(id) {
+
   }
 }
 
--- database/migrations/1592524484145_games_schema.js
+++ database/migrations/1592524484145_games_schema.js
@@ -7,7 +7,7 @@ class GamesSchema extends Schema {
   up () {
     this.create('games', (table) => {
       table.increments()
-      table.string('status', 8).defaultTo('inactive') // active | inactive
+      table.string('status', 9).defaultTo('inactive') // preparing | playing
     })
   }
 
