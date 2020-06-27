dd2b2b4714fa520941adac28986fb9978d233898
egsded <sarcasmo1601@gmail.com>
2020-06-27 16:56:40 -0500

    cambio de migraciones

--- database/migrations/1592525269767_games_has_cards_schema.js
+++ database/migrations/1592525269767_games_has_cards_schema.js
@@ -6,8 +6,9 @@ const Schema = use('Schema')
 class GamesHasCardsSchema extends Schema {
   up () {
     this.create('games_has_cards', (table) => {
-      table.integer('games_id').unsigned().references('id').inTable('games')
-      table.integer('cards_id').unsigned().references('id').inTable('cards')
+    	table.increments()
+    	table.integer('games_id').unsigned().references('id').inTable('games')
+    	table.integer('cards_id').unsigned().references('id').inTable('cards')
     })
   }
 
